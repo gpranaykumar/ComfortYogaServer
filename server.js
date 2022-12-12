1248a97068c2c1626da0ffd19ca9dc8bf55ec9f5
gpranaykumar <65266203+gpranaykumar@users.noreply.github.com>
2022-12-12 14:13:47 +0530

    .

--- server.js
+++ server.js
@@ -18,10 +18,11 @@ const PORT = process.env.PORT || 4000
 app.use(cors())
   app.use((req,res,next) => {
     res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
-  next();
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+    next();
 })
 app.get('/hello', function(req, res){
     res.send("Hello World!");
